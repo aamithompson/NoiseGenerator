3ac1ed22dd6d63268d0b5b2e671eb3d49ebbadd1
aamithompson <aamithompson@gmail.com>
2026-06-08 08:08:34 -0700

    update: server health GET api

--- Server/lambda_function.py
+++ Server/lambda_function.py
@@ -2,6 +2,19 @@ import LambdaHandlers as lh
 import json
 
 def lambda_handler(event, context):
+    #Server Health Check
+    method = event.get("requestContext", {}).get("http", {}).get("method")
+    path = event.get("requestContext", {}).get("http", {}).get("path")
+
+    if method == "GET" and path == "/health":
+        return {
+            "statusCode": 200,
+            "headers": {"Content-Type": "application/json"},
+            "body": json.dumps({"status": "ok"})
+        }
+
+
+    #Noise Return Functions
     body = json.loads(event.get("body", "{}"))
     noiseClass = body.get("noiseClass")
 
